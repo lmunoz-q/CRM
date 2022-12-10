1569db0fc9a7ef40d80379ac1385cdee826e883b
aorus <lmunoz-q@student.42.fr>
2022-12-10 23:36:07 +0100

    add doc google

--- srcs/user.py
+++ srcs/user.py
@@ -1,21 +1,34 @@
 import faker
+import logging
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+logging.basicConfig(filename=BASE_DIR / 'user.log', level=logging.WARNING)
 
 fake = faker.Faker()
 
 def get_user():
-	""" Generate a single user
-	:return: str: user
 	"""
+	Generate a user "{first_name} {last_name}" from Faker module
+	Returns: str
+
+	"""
+	logging.info("Generating user.")
 	return f"{fake.first_name()} {fake.last_name()}"
 
 def get_users(n):
-	"""Generate a list of users
-	:param n: int: number of user to generate
-	:return: list[str]: users
+	"""
+	Generate a list of {n} user
+	Args: number of user to return
+		n: int
+
+	Returns: list
+
 	"""
 	users = []
 	for i in range(n):
 		users.append(get_user())
+	logging.info(f"Generating a list of {n} user.")
 	return users
 
 if __name__ == "__main__":
