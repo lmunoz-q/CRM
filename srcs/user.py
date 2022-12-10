340a63195d89b9235890c41e846200ebe6823336
aorus <lmunoz-q@student.42.fr>
2022-12-10 21:56:48 +0100

    ajout docstring

--- srcs/user.py
+++ srcs/user.py
@@ -3,15 +3,21 @@ import faker
 fake = faker.Faker()
 
 def get_user():
+	""" Generate a single user
+	:return: str: user
+	"""
 	return f"{fake.first_name()} {fake.last_name()}"
 
 def get_users(n):
+	"""Generate a list of users
+	:param n: int: number of user to generate
+	:return: list[str]: users
+	"""
 	users = []
 	for i in range(n):
 		users.append(get_user())
-
 	return users
 
 if __name__ == "__main__":
-	user = get_user()
+	user = get_users(n=10)
 	print(user)
